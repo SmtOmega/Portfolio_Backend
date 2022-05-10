77ef4e47a91058c13eae2e925ae02bc92c5451ac
Shola Taiwo <m_webmaster007@yahoo.co.uk>
2022-05-10 08:24:50 +0100

    remove the register route

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,16 +4,6 @@ const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
-router.post("/user", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    const token = await user.generateAuthToken();
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send();
-  }
-});
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
